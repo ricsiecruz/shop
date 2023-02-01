2f2157ae4c825a55ea52269ae83c0de7a46ae68e
ricsiecruz <ricsie_aguila@yahoo.com>
2023-02-01 16:10:13 +0800

    no message

--- src/app/components/admin/products/products.component.ts
+++ src/app/components/admin/products/products.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  dataList: any;
+
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+    this.httpClient.get('https://dummyjson.com/products').subscribe((res: any) => {
+      this.dataList = res.products;
+    })
   }
 
 }
